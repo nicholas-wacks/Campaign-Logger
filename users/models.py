62ce66038fc525c869828d5a035deaa5fba82172
Nicholas Wacks <nicholas.wacks@gmail.com>
2018-10-20 22:57:31 -0600

    Added custom user type

---
+++ users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import AbstractUser
+
+# Per official Django docs, following suggestion to make a custom user type in case of future needs
+# For more details, see: https://docs.djangoproject.com/en/2.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+class User(AbstractUser):
+    pass
